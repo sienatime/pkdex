91cb246820168b5c213ecbfb7c085ea878ac4731
Siena Aguayo <siena.aguayo@gmail.com>
2016-06-14 13:56:08 -0700

    converted PokemonListAdapter into RecyclerView

--- app/src/main/java/com/siena/pokedex/adapters/PokemonListAdapter.java
+++ app/src/main/java/com/siena/pokedex/adapters/PokemonListAdapter.java
@@ -4,11 +4,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -19,11 +19,7 @@ import com.siena.pokedex.R;
 import com.siena.pokedex.fragments.PokeInfoFragment;
 import com.siena.pokedex.models.Pokemon;
 import com.squareup.picasso.Picasso;
-import io.realm.Realm;
 import io.realm.RealmResults;
-import io.realm.Sort;
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.siena.pokedex.PokemonUtil.formatId;
 import static com.siena.pokedex.PokemonUtil.getPokeString;
@@ -32,110 +28,68 @@ import static com.siena.pokedex.PokemonUtil.getPokemonImageId;
 /**
  * Created by Siena Aguayo on 12/27/14.
  */
-public class PokemonListAdapter extends BaseAdapter {
-  private List<Row> rows = new ArrayList<>();
-  private final int POKEMON_ROW = 100;
+public class PokemonListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+  private RealmResults<Pokemon> pokemon;
+  private Context context;
+  private Picasso picasso;
 
-  public PokemonListAdapter(Context context) {
-    Realm realm = Realm.getDefaultInstance();
-    RealmResults<Pokemon> pokes = realm.allObjectsSorted(Pokemon.class, "id", Sort.ASCENDING);
-
-    for (Pokemon poke : pokes) {
-      rows.add(new PokeRow(POKEMON_ROW, poke, context));
-    }
+  public PokemonListAdapter(Context context, RealmResults<Pokemon> pokes) {
+    this.pokemon = pokes;
+    this.context = context;
+    this.picasso = Picasso.with(context);
   }
 
-  @Override public int getCount() {
-    return rows.size();
+  @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View v = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.row_pokemon_item, parent, false);
+    PokeRowViewHolder vh = new PokeRowViewHolder(v);
+    return vh;
   }
 
-  @Override public Row getItem(int position) {
-    return rows.get(position);
-  }
-
-  @Override public long getItemId(int position) {
-    return position;
-  }
-
-  @Override public View getView(int position, View convertView, ViewGroup parent) {
-    return rows.get(position).getView(convertView, parent);
-  }
-
-  @Override public int getItemViewType(int position) {
-    return rows.get(position).getType();
-  }
-
-  @Override public int getViewTypeCount() {
-    return 1;
-  }
-
-  public static class PokeRow implements Row {
-    private Pokemon pokemon;
-    private int rowType;
-    private Context context;
-    private Picasso picasso;
-
-    public PokeRow(int rowType, Pokemon pokemon, Context context) {
-      this.pokemon = pokemon;
-      this.rowType = rowType;
-      this.context = context;
-      this.picasso = Picasso.with(context);
-    }
-
-    @Override public int getType() {
-      return rowType;
-    }
-
-    @Override public View getView(View convertView, ViewGroup parent) {
-      ViewHolder viewHolder;
-      if (convertView == null) {
-        convertView =
-            LayoutInflater.from(context).inflate(R.layout.row_pokemon_item, parent, false);
-        viewHolder = new ViewHolder(convertView);
-        convertView.setTag(viewHolder);
-      } else {
-        viewHolder = (ViewHolder) convertView.getTag();
+  @Override public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, int position) {
+    final PokeRowViewHolder pokeRowViewHolder = (PokeRowViewHolder) viewHolder;
+    final Pokemon pokemon = this.pokemon.get(position);
+    pokeRowViewHolder.container.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Fragment fragment = new PokeInfoFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(PokemonUtil.POKEMON_ID_KEY, pokemon.getId());
+        fragment.setArguments(bundle);
+        ((ActionBarActivity) context).getSupportFragmentManager()
+            .beginTransaction()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(R.id.container, fragment)
+            .addToBackStack(null)
+            .commit();
       }
+    });
 
-      viewHolder.container.setOnClickListener(new View.OnClickListener() {
-        @Override public void onClick(View v) {
-          Fragment fragment = new PokeInfoFragment();
-          Bundle bundle = new Bundle();
-          bundle.putInt(PokemonUtil.POKEMON_ID_KEY, pokemon.getId());
-          fragment.setArguments(bundle);
-          ((ActionBarActivity) context).getSupportFragmentManager()
-              .beginTransaction()
-              .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-              .replace(R.id.container, fragment)
-              .addToBackStack(null)
-              .commit();
-        }
-      });
+    pokeRowViewHolder.pokeId.setText(formatId(pokemon));
+    pokeRowViewHolder.pokeName.setText(getPokeString(pokemon.getId(), "pokemon_species_name_"));
 
-      viewHolder.pokeId.setText(formatId(pokemon));
-      viewHolder.pokeName.setText(getPokeString(pokemon.getId(), "pokemon_species_name_"));
-
-      int imageId = getPokemonImageId(pokemon);
-      if (imageId > 0) {
-        viewHolder.pokeImage.setVisibility(View.VISIBLE);
-        picasso.load(imageId).into(viewHolder.pokeImage);
-      } else {
-        Log.e("listadapter", "couldn't find image for id " + Integer.toString(pokemon.getId()));
-        viewHolder.pokeImage.setVisibility(View.INVISIBLE);
-      }
-
-      return convertView;
+    int imageId = getPokemonImageId(pokemon);
+    if (imageId > 0) {
+      pokeRowViewHolder.pokeImage.setVisibility(View.VISIBLE);
+      picasso.load(imageId).into(pokeRowViewHolder.pokeImage);
+    } else {
+      Log.e("listadapter", "couldn't find image for id " + Integer.toString(pokemon.getId()));
+      pokeRowViewHolder.pokeImage.setVisibility(View.INVISIBLE);
     }
+  }
 
-    static class ViewHolder {
-      @InjectView(R.id.row_poke_id) TextView pokeId;
-      @InjectView(R.id.row_poke_name) TextView pokeName;
-      @InjectView(R.id.row_poke_image) ImageView pokeImage;
-      @InjectView(R.id.poke_row) RelativeLayout container;
+  @Override public int getItemCount() {
+    return pokemon.size();
+  }
 
-      public ViewHolder(View source) {
-        ButterKnife.inject(this, source);
-      }
+  public static class PokeRowViewHolder extends RecyclerView.ViewHolder {
+    @InjectView(R.id.row_poke_id) TextView pokeId;
+    @InjectView(R.id.row_poke_name) TextView pokeName;
+    @InjectView(R.id.row_poke_image) ImageView pokeImage;
+    @InjectView(R.id.poke_row) RelativeLayout container;
+
+    public PokeRowViewHolder(View source) {
+      super(source);
+      ButterKnife.inject(this, source);
     }
   }
 }
--- app/src/main/java/com/siena/pokedex/fragments/PokeListFragment.java
+++ app/src/main/java/com/siena/pokedex/fragments/PokeListFragment.java
@@ -1,14 +1,20 @@
 package com.siena.pokedex.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import com.siena.pokedex.R;
 import com.siena.pokedex.adapters.PokemonListAdapter;
+import com.siena.pokedex.models.Pokemon;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Created by Siena Aguayo on 12/27/14.
@@ -21,8 +27,15 @@ public class PokeListFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_pokemon_listview, container, false);
-    ((ListView) rootView.findViewById(R.id.poke_listview)).setAdapter(
-        new PokemonListAdapter(getActivity()));
+    Realm realm = Realm.getDefaultInstance();
+    RealmResults<Pokemon> allPokemon = realm.allObjectsSorted(Pokemon.class, "id", Sort.ASCENDING);
+    Context activityContext = getActivity();
+
+    RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.poke_listview);
+    recyclerView.setHasFixedSize(true);
+    recyclerView.setAdapter(new PokemonListAdapter(activityContext, allPokemon));
+    recyclerView.setLayoutManager(new LinearLayoutManager(activityContext));
+
     ((ActionBarActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     return rootView;
   }
